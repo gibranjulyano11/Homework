e497e149b5973aafc626b3aa278ec6bff702c68e
gibranjulyano11 <gibranjulyano11@gmail.com>
2020-06-23 21:28:47 -0500

    tarea

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace AplicacionConsola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                Console.WriteLine(args[0]);
+            }
+            Console.Write("Hola soy Doki, ");
+            Console.WriteLine("Cúal es tu nombre?");
+            string datosIngresados = Console.ReadLine();
+            Console.WriteLine("Mucho gusto " + datosIngresados + "," + "¿Cómo estás?");
+            string datosReceptados = Console.ReadLine();
+            Console.Write("Cuídate, hasta luego ");
+            Console.WriteLine(datosIngresados);
+        }
+    }
+}
